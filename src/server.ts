9c58f119673173d25b6ce551bcc555adfbb2bc0f
Stas Raranetskyi <rar.stas@gmail.com>
2025-09-02 20:32:59 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import env from './env';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const app = express();
+
+const targets: Record<string, string> = {
+	'buildon-proxy.vercel.app':
+		'https://buildon-develop-7mwbcnhl4a-ew.a.run.app',
+	'buildon.tech': 'https://buildon-develop-7mwbcnhl4a-ew.a.run.app',
+	'*.buildon.tech': 'https://buildon-develop-7mwbcnhl4a-ew.a.run.app',
+};
+
+app.use((req, res, next) => {
+	const host = req.headers.host || '';
+
+	for (const [key, target] of Object.entries(targets)) {
+		if (host.endsWith(key)) {
+			return createProxyMiddleware({
+				target,
+				changeOrigin: true,
+			})(req, res, next);
+		}
+	}
+
+	res.status(404).send('Domain not configured');
+});
+
+app.listen(env.PORT, () => {
+	console.log(`Server running on port ${env.PORT}`);
+});
