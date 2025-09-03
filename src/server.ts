a2ca7c659ec8b8c501cc5d348f62cbe9518af871
Stas Raranetskyi <rar.stas@gmail.com>
2025-09-03 19:47:32 +0100

    feat: improve logic

--- src/server.ts
+++ src/server.ts
@@ -11,15 +11,25 @@ const targets: Record<string, string> = {
 	'buildon.app': 'https://buildon-7mwbcnhl4a-ew.a.run.app',
 };
 
+const proxyMap = Object.fromEntries(
+	Object.entries(targets).map(([host, target]) => [
+		host,
+		createProxyMiddleware({
+			target,
+			changeOrigin: true,
+			xfwd: true,
+			secure: true,
+			ws: true,
+		}),
+	]),
+);
+
 app.use((req, res, next) => {
 	const host = req.headers.host || '';
 
-	for (const [key, target] of Object.entries(targets)) {
+	for (const [key, proxy] of Object.entries(proxyMap)) {
 		if (host.endsWith(key)) {
-			return createProxyMiddleware({
-				target,
-				changeOrigin: true,
-			})(req, res, next);
+			return proxy(req, res, next);
 		}
 	}
 
